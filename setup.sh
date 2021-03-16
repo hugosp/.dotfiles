803deadbb484dc80f7efcfb72b854e319b3b993a
hugosp <stefan.sundell@gmail.com>
2021-03-16 08:15:56 +0100

    add vim to dotfiles

--- setup.sh
+++ setup.sh
@@ -5,10 +5,17 @@
 #cp tmux-theme.sh ./tmux-powerline/themes/default.sh
 
 ### SYMLINKS
+echo "Adding Symlinks"
 [[ -f  ~/.tmux.conf ]] || ln -sv ~/.dotfiles/.tmux.conf ~
+[[ -f  ~/.vim ]] || ln -sv ~/.dotfiles/vim/ ~/.vim
 
 ## ADD READ BASH-EXTRAS TO .bashrc
- cat ~/.bashrc | grep -qF 'source ~/.dotfiles/.bash_profile' || echo 'source ~/.dotfiles/.bash_profile' >> ~/.bashrc
+echo "Adding alias and functions to bashrc"
+cat ~/.bashrc | grep -qF 'source ~/.dotfiles/.bash_profile' || echo 'source ~/.dotfiles/.bash_profile' >> ~/.bashrc
+
+ ## Add vundle to vim
+echo "Adding vundle to vim - Remember to run vundle install first time."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/vim/bundle/Vundle.vim
 
 
 
---
+++ vim/plugins.vim
@@ -0,0 +1,24 @@
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'scrooloose/nerdtree'
+Plugin 'tpope/vim-vinegar'
+Plugin 'ctrlpvim/ctrlp.vim'
+Plugin 'travisjeffery/vim-auto-mkdir'
+Plugin 'jiangmiao/auto-pairs'
+Plugin 'posva/vim-vue'
+Plugin 'jelera/vim-javascript-syntax'
+Plugin 'othree/html5.vim'
+Plugin 'hail2u/vim-css3-syntax'
+Plugin 'rking/ag.vim'
+Plugin 'tpope/vim-surround'
+Plugin 'StanAngeloff/php.vim'
+Plugin 'ervandew/supertab'
+
+
+call vundle#end()            " required
+filetype plugin indent on    " required
+

469c98247131207177641ecd03d042795cba5e3b
hugoup <stefan@unitedprofile.se>
2021-06-09 21:58:41 +0200

    add alias in zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -8,8 +8,9 @@ chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+[[ -e  ~/.zshrc ]] || ln -sv ~/.dotfiles/zsh/.zshrc ~
+

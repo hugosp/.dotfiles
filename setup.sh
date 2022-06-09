021932415e5b113bd86cd1e59a188ac22c4c2e78
hugosp <stefan.sundell@gmail.com>
2022-06-09 18:17:39 +0200

    miff and moff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+## Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "$($HOME/.linuxbrew/bin/brew shellenv)" >> $HOME/.profile
+$HOME/.linuxbrew/bin/brew shellenv
+
+## Install tools
+brew install stow
+brew install bat
+brew install diff-so-fancy
+#brew install magic-wormhole
+brew install hstr
+
+## Add hstr to bash/zsh
+hstr --show-configuration >> ~/.bashrc && . ~/.bashrc
+hstr --show-configuration >> ~/.zshrc && . ~/.zshrc
+
+
 ### SYMLINKS
 echo "* Adding Symlinks"
 [[ -e  ~/.tmux.conf ]] || ln -sv ~/.dotfiles/tmux/.tmux.conf ~
@@ -10,6 +27,7 @@ echo "* Adding Symlinks"
 [[ -e  ~/.gitconfig ]] || ln -sv ~/.dotfiles/git/.gitconfig ~
 [[ -e  ~/.gitignore_global ]] || ln -sv ~/.dotfiles/git/.gitignore_global ~
 
+
 ## ADD READ BASH-EXTRAS TO .bashrc
 echo "* Adding alias and functions to bashrc"
 cat ~/.bashrc | grep -qF 'source ~/.dotfiles/bash/.bash_profile' || echo 'source ~/.dotfiles/bash/.bash_profile' >> ~/.bashrc
@@ -17,23 +35,3 @@ cat ~/.bashrc | grep -qF 'source ~/.dotfiles/bash/.bash_profile' || echo 'source
 ## Add vundle to vim
 echo "* Adding vundle to vim - Remember to run vundle install first time."
 [[ -d ~/.dotfiles/vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/vim/bundle/Vundle.vim
-
-## Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# echo 'eval "$(/home/hugo/.linuxbrew/bin/brew shellenv)"' >> /home/hugo/.profile
-# eval "$(/home/hugo/.linuxbrew/bin/brew shellenv)"
-
-echo "$($HOME/.linuxbrew/bin/brew shellenv)" >> $HOME/.profile
-
-$HOME/.linuxbrew/bin/brew shellenv
-
-## tools
-brew install bat
-brew install diff-so-fancy
-brew install magic-wormhole
-brew install hstr
-
-## Add hstr to bash/zsh
-hstr --show-configuration >> ~/.bashrc && . ~/.bashrc
-hstr --show-configuration >> ~/.zshrc && . ~/.zshrc

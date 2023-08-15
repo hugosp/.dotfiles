b721ca448bd167aea34ec1b9e083cf04e62e4384
Hewgo <stefan.sundell@gmail.com>
2023-08-15 19:39:48 +0200

    i3 config

---
+++ i3/.config/i3/hemma.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 3840x1288 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --off --output DVI-I-1-1 --off
